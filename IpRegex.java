73bac396b94d30a76bcfd58ae81da56403c5c082
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-05-18 15:01:05 +0300

    Add files via upload

---
+++ IpRegex.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IpRegex {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+
+    static class MyRegex {
+        public String pattern = "((\\d|\\d\\d|[0-1]\\d\\d|2[0-4][0-9]|25[0-5])\\.){3}(\\d|\\d\\d|[0-1]\\d\\d|2[0-4][0-9]|25[0-5])";
+    }
+}
